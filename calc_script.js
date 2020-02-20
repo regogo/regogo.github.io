539c1936619927c693b756f763b6afc0cfbe597e
Henry <regondolajosh@gmail.com>
2020-02-20 14:31:30 +0800

    added js and onclicks

---
+++ calc_script.js
@@ -0,0 +1,27 @@
+<script>
+
+  function back (calculator) {
+    size = calculator.display.value.length;
+    calculator.display.value = calculator.display.value.substring(0, size-1);
+  }
+
+  function clear() {
+    size = calculator.display.value.length;
+    calculator.display.value = ''
+  }
+
+  function process (calculator) {
+
+    if (calculator.display.value.includes("%")) {
+
+      size = calculator.display.value.length;
+      n = Number(calculator.display.value.substring(0, size-1));
+      calculator.display.value = n/100;
+    } else {
+
+      calculator.display.value = eval(calculator.display.value);
+
+    }
+}
+
+</script>
