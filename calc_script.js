752ba436ff06689db08fda552bb9654b435d7eae
Henry <regondolajosh@gmail.com>
2020-02-20 14:40:02 +0800

    updated scripts

--- calc_script.js
+++ calc_script.js
@@ -1,16 +1,16 @@
 <script>
 
-  function back (calculator) {
+  function back(calculator) {
     size = calculator.display.value.length;
     calculator.display.value = calculator.display.value.substring(0, size-1);
   }
 
-  function clear() {
+  function clear(calculator) {
     size = calculator.display.value.length;
     calculator.display.value = ''
   }
 
-  function process (calculator) {
+  function process(calculator) {
 
     if (calculator.display.value.includes("%")) {
 
@@ -18,9 +18,7 @@
       n = Number(calculator.display.value.substring(0, size-1));
       calculator.display.value = n/100;
     } else {
-
       calculator.display.value = eval(calculator.display.value);
-
     }
 }
 
