19442c90c781111e42e9cf6c8ba8ab887ff86cd9
Henry <regondolajosh@gmail.com>
2020-02-20 15:33:51 +0800

    added comments

--- calc_script.js
+++ calc_script.js
@@ -1,12 +1,15 @@
+//backspace
   function back(calculator) {
     size = calculator.display.value.length;
     calculator.display.value = calculator.display.value.substring(0, size-1);
   }
 
+//clear input
   function clear(calculator) {
     calculator.display.value = '';
   }
 
+//to evaluate the inputs
   function process(calculator) {
     try {
         calculator.display.value = eval(calculator.display.value);
