dffa047c52459b67059a8ec1349face1a6aa2b49
Henry <regondolajosh@gmail.com>
2020-02-20 15:05:28 +0800

    removed unused scripts

--- calc_script.js
+++ calc_script.js
@@ -1,25 +1,12 @@
-<script>
-
   function back(calculator) {
     size = calculator.display.value.length;
     calculator.display.value = calculator.display.value.substring(0, size-1);
   }
 
   function clear(calculator) {
-    size = calculator.display.value.length;
-    calculator.display.value = ''
+    calculator.display.value = '';
   }
 
   function process(calculator) {
-
-    if (calculator.display.value.includes("%")) {
-
-      size = calculator.display.value.length;
-      n = Number(calculator.display.value.substring(0, size-1));
-      calculator.display.value = n/100;
-    } else {
       calculator.display.value = eval(calculator.display.value);
-    }
-}
-
-</script>
+  }
